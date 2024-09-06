23c4799ded0975f0a16aaa2cfc392f0a49d8f5e2
OdinEM <e.oden@alustudent.com>
2024-09-06 23:28:00 +0200

    addition

---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,13 @@
+﻿cat <<EOL > Program.cs
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Print the message to the console
+        Console.WriteLine("Programming is like building a multilingual puzzle");
+    }
+}
+
+

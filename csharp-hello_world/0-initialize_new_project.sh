4e50c076eea3c9afd0d4bc2f83600d90abf9d65e
OdinEM <e.oden@alustudent.com>
2024-09-06 13:49:45 +0200

    first commit

---
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a new directory named 0-new_project
+mkdir -p 0-new_project
+
+# Navigate into the 0-new_project directory
+cd 0-new_project
+
+# Initialize a new C# console project
+dotnet new console

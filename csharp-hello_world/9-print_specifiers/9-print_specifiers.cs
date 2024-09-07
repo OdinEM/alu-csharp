7e9f9b6a8fd449db6e080c6130e866d282d485c0
OdinEM <e.oden@alustudent.com>
2024-09-07 14:52:27 +0200

    looop

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 class Program {
     static void Main(string[] args) {
-        int caseNumber = 1; 
+        int caseNumber = 1; int caseNumber = 2; int caseNumber = 3; 
   double percent = 0.00;  double amount = 0.00;
         // Set values based on the caseNumber
         if (caseNumber == 1) { percent = -44.98; amount = -9999.99; }

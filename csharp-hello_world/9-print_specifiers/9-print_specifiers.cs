f3ec359086699525e409c90a7457f38158e72d04
OdinEM <e.oden@alustudent.com>
2024-09-07 13:37:19 +0200

    share

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 class Program {
-    static void Main(string[] args) {
-        double percent; double amount;
-        percent = -44.98;   amount = -9999.99;
+    static void Main(string[] args)
+    {
+        int caseNumber = 1; double percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00; double amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;
+        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}"); 
+        caseNumber = 2; percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00; amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;
         Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
-        percent = 26.41;  amount = 12345.68;
-        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
-        percent = 0.00;  amount = 0.00;
+        caseNumber = 3; percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00;  amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;       
         Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
     }
 }

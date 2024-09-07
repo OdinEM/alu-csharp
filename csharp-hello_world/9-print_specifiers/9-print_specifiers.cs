63ff6ccf52794af5fab31617637eacbb463da92b
OdinEM <e.oden@alustudent.com>
2024-09-07 13:30:17 +0200

    done

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-class Program
-{
-    static void Main(string[] args)
-    {
-        int caseNumber = 1; // 1: Negative, 2: Positive, 3: Zero
-        double percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00;
-        double amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;
-        Console.WriteLine($"Percent: {percent:0.00}%");
-        Console.WriteLine($"Currency: {amount:C}");
+class Program {
+    static void Main(string[] args) {
+        double percent; double amount;
+        percent = -44.98;   amount = -9999.99;
+        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
+        percent = 26.41;  amount = 12345.68;
+        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
+        percent = 0.00;  amount = 0.00;
+        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
     }
 }

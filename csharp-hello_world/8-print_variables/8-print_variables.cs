f769d2d1ec6e46de4711c1c8a214a2fff69f7f31
OdinEM <e.oden@alustudent.com>
2024-09-07 12:20:36 +0200

    sent

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -3,10 +3,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Assume these variables are pre-defined and not editable
+        // Initialize variables with the required values
         string str1 = "972";
         string str2 = "Mission Street";
+
         Console.WriteLine(str1 + " " + str2);   // Output: "972 Mission Street"
-        Console.WriteLine(); // Placeholder to meet the line count requirement
     }
 }

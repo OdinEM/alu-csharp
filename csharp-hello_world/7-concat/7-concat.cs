329d52d907a9e004d209535b96ca459107d79645
OdinEM <e.oden@alustudent.com>
2024-09-07 11:27:43 +0200

    updated again

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         string str1 = "Welcome to";
-        string str2 = " Mission Street!";
-        string result = str1 + str2; // Concatenate str1 and str2
-        Console.WriteLine(result);   // Print the result
+        string str2 = " Holberton School!";
+        string str3 = " Mission Street!";
+        Console.WriteLine(str1 + str2);
+     Console.WriteLine(str1 + str3);
     }
 }

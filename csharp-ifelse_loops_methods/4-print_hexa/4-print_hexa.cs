0680bd2114beaec662ca3318b2f3f9324756a9a7
OdinEM <e.oden@alustudent.com>
2024-09-07 19:05:23 +0200

    third

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an empty string to hold the output
+        string result = "";
+
+        // Use a loop to generate numbers from 0 to 98
+        for (int i = 0; i <= 98; i++)
+        {
+            // Append the number in decimal and hexadecimal format
+            result += i.ToString() + " = " + i.ToString("X") + "\n";
+        }
+
+        // Print the result string in one go
+        Console.Write(result);
+    }
+}

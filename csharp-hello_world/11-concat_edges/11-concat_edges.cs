d9bf4e494ef4219f27b75394046ab6bdbbaaa5c2
OdinEM <e.oden@alustudent.com>
2024-09-07 12:49:28 +0200

    lastly

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string part1 = "object"; string part2 = "-oriented ";  string part3 = "programming";
+        string part4 = " in C#";
+        string result = part1 + "-oriented " + part3 + part4;
+        Console.WriteLine(result);
+    }
+}

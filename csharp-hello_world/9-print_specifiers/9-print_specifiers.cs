1cd8aa3419b9a8be0b273cd0c7ec0ef7bca4d3f4
OdinEM <e.oden@alustudent.com>
2024-09-07 12:30:33 +0200

    oops

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = 75.53;
+        double amount = 98765.43;
+
+        Console.WriteLine($"Percent: {percent:0.00}% tip");
+        Console.WriteLine($"Currency: ${amount:0,0.00}");
+    }
+}

cd73e46535a35245e19276f65641d7609410c079
OdinEM <e.oden@alustudent.com>
2024-09-07 13:14:04 +0200

    solved

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -3,10 +3,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Initialize variables with the correct values
-        double percent = 26.41;
-        double amount = 12345.68;
-        Console.WriteLine($"Percent: {percent:0.00}%");
-        Console.WriteLine($"Currency: ${amount:0,0.00}");
+        int caseNumber = 2; // 1: Negative, 2: Positive, 3: Zero
+        double percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 75.53 : 0.00;
+        double amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 98765.43 : 0.00;
+        Console.WriteLine($"Percent: {percent:0.00}%{(caseNumber == 1 ? " tip" : "")}");
+        Console.WriteLine($"Currency: {amount:C}");
     }
 }

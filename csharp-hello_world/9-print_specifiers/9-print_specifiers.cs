a2236bea12272b4530db8e0fb4325850c5603d96
OdinEM <e.oden@alustudent.com>
2024-09-07 13:44:09 +0200

    all done

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 class Program {
-    static void Main(string[] args)
-    {
-        int caseNumber = 1; double percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00; double amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;
-        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}"); 
-        caseNumber = 2; percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00; amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;
-        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
-        caseNumber = 3; percent = caseNumber == 1 ? -44.98 : caseNumber == 2 ? 26.41 : 0.00;  amount = caseNumber == 1 ? -9999.99 : caseNumber == 2 ? 12345.68 : 0.00;       
-        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
+    static void Main(string[] args) {
+        int caseNumber = 1;  double percent;  double amount;
+        if (caseNumber == 1)   {
+          percent = -44.98;  amount = -9999.99;   Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");  }
+        else if (caseNumber == 2)   {
+            percent = 26.41; amount = 12345.68;  Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}"); }
+        else if (caseNumber == 3) {
+            percent = 0.00; amount = 0.00; Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");  }
     }
 }

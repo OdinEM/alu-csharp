cbac336881fdd8f161c7a88f0e59f0ed392de37f
OdinEM <e.oden@alustudent.com>
2024-09-07 15:09:10 +0200

    done

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int number = random.Next(-100, 101);
+
+        Console.Write($"{number} ");
+
+        if (number > 0)
+        {
+            Console.WriteLine("is positive");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("is negative");
+        }
+        else
+        {
+            Console.WriteLine("is zero");
+        }
+    }
+}
+

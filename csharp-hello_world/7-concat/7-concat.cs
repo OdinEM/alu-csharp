f03e2499df7c374bb7f5ce9a35af7745156a718a
OdinEM <e.oden@alustudent.com>
2024-09-07 11:11:06 +0200

    Add 7-concat project

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Welcome to";
+        string str2 = "Holberton School!";
+        string result = str1 + " " + str2; // Concatenate str1, a space, and str2
+        Console.WriteLine(result); // Print the result
+    }
+}
+

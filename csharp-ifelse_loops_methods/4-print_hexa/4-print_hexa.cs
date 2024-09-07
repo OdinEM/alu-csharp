2cf1babc0f88d19f86e3410d926f31bbc9be224e
OdinEM <e.oden@alustudent.com>
2024-09-07 19:57:14 +0200

    task

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -7,8 +7,11 @@ class Program
         // Use a loop to generate numbers from 0 to 98
         for (int i = 0; i <= 98; i++)
         {
-            // Print each number in decimal and hexadecimal format
-            Console.Write($"{i} = {i:X}\n");
+            // Only print '1' if the current number is 1
+            if (i == 1)
+            {
+                Console.Write("1");
+            }
         }
     }
 }

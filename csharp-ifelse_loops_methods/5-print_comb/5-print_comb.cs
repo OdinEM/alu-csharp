9a9034946e4703e9b877330c3cf4b86108492bd8
OdinEM <e.oden@alustudent.com>
2024-09-07 20:08:06 +0200

    drone

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Use a loop to generate numbers from 0 to 99
+        for (int i = 0; i < 100; i++)
+        {
+            // Print each number with leading zero if necessary and followed by ", " unless it's the last number
+            Console.Write(i.ToString("D2"));
+            
+            // Only print ", " if it's not the last number
+            if (i < 99)
+            {
+                Console.Write(", ");
+            }
+        }
+        
+        // Print a newline at the end
+        Console.WriteLine();
+    }
+}

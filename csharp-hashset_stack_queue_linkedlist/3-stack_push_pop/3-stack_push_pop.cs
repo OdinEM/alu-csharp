598226aef87540f22d7318954469a5ff3b3bcb98
OdinEM <e.oden@alustudent.com>
2024-09-27 22:27:27 +0200

    door

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,58 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace StackImplementation
+class MyStack<T>
 {
-    public class MyStack<T>
+    private List<T> stackList = new List<T>();
+
+    public int Count()
     {
-        private List<T> stack = new List<T>();
-
-        public void Push(T item)
-        {
-            stack.Add(item);
-        }
-
-        public T Pop()
-        {
-            if (stack.Count == 0)
-            {
-                throw new InvalidOperationException("Stack is empty");
-            }
-
-            T item = stack[stack.Count - 1];
-            stack.RemoveAt(stack.Count - 1);
-            return item;
-        }
-
-        public int Count
-        {
-            get { return stack.Count; }
-        }
+        return stackList.Count;
     }
 
-    public class Program
+    public void Push(T obj)
     {
-        public static void Main(string[] args)
+        stackList.Add(obj);
+    }
+
+    public T Pop()
+    {
+        if (stackList.Count == 0)
         {
-            MyStack<int> stack = new MyStack<int>();
-
-            Console.WriteLine("Pushing elements onto the stack:");
-            for (int i = 1; i <= 5; i++)
-            {
-                stack.Push(i);
-                Console.WriteLine($"Pushed: {i}");
-            }
-
-            Console.WriteLine($"\nStack count: {stack.Count}");
-
-            Console.WriteLine("\nPopping elements from the stack:");
-            while (stack.Count > 0)
-            {
-                int popped = stack.Pop();
-                Console.WriteLine($"Popped: {popped}");
-            }
-
-            Console.WriteLine($"\nFinal stack count: {stack.Count}");
+            return default(T);
         }
+        T item = stackList[stackList.Count - 1];
+        stackList.RemoveAt(stackList.Count - 1);
+        return item;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MyStack<int> stack = new MyStack<int>();
+        stack.Push(1);
+        Console.WriteLine(stack.Count());
     }
 }

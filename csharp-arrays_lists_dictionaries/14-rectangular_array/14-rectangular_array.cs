457035fb77acecbdf4bea01a20d391417d690765
OdinEM <e.oden@alustudent.com>
2024-09-27 20:07:52 +0200

    test

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -15,12 +15,21 @@ class Program
         {
             for (int j = 0; j < 5; j++)
             {
-                Console.Write(array[i, j] + " ");
+                // Print each element followed by a space
+                Console.Write(array[i, j]);
+
+                // Add space after each element except the last element of the row
+                if (j < 4)
+                {
+                    Console.Write(" ");
+                }
             }
-            Console.WriteLine(); // Add a line break after each row
+
+            // Add a space after the last element in the row before the newline
+            Console.WriteLine(" "); 
         }
 
-        // Add an extra line break to make total output 56 characters
-        Console.WriteLine(); // This will ensure a total of 56 chars with the new line
+        // Extra line to match the expected 56-character output
+        Console.WriteLine(); 
     }
 }

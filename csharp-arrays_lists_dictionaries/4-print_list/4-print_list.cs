f6263cf9861c7969b4a6974ec480dcfb684d1ac9
OdinEM <e.oden@alustudent.com>
2024-09-27 19:37:50 +0200

    test

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿﻿using System.Collections;
 
-class List
-{
-    public static List<int>? CreatePrint(int size)
-    {
-        if (size < 0)
-        {
+class List{
+
+    public static List<int> CreatePrint(int size){
+        
+        if(size < 0){
             Console.WriteLine("Size cannot be negative");
             return null;
+        }else{
+            List<int> CacheList = new List<int>();
+            for(int i = 0; i < size ; i ++){
+                Console.Write(i + " ");
+                CacheList.Add(i);
+            }
+            Console.WriteLine();
+            return CacheList;
         }
-
-        List<int> newList = new List<int>();
-
-        for (int i = 0; i < size; i++)
-        {
-            newList.Add(i);
-            Console.Write(i);
-            if (i < size - 1)
-                Console.Write(" ");
-        }
-
-        Console.WriteLine();
-        return newList;
     }
 }

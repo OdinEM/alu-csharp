afd9b35fbe3cc61d710cac986f52201fce29c7ca
OdinEM <e.oden@alustudent.com>
2024-09-27 20:55:15 +0200

    three

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        // Print the number of items in the stack
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        // Print the top item or indicate that the stack is empty
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        // Check if the stack contains the search item
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
+
+        // Remove items up to and including the search item if found
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+
+            // Pop items until we find the search item
+            while (aStack.Count > 0)
+            {
+                string item = aStack.Pop();
+                if (item == search)
+                {
+                    break; // Stop when we find the search item
+                }
+                tempStack.Push(item); // Store items in tempStack
+            }
+
+            // Restore items back to aStack
+            while (tempStack.Count > 0)
+            {
+                aStack.Push(tempStack.Pop());
+            }
+        }
+
+        // Add the new item to the stack
+        aStack.Push(newItem);
+
+        // Return the modified stack
+        return aStack;
+    }
+}

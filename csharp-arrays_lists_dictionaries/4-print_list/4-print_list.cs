a1b5af9790fed8f507c4fe1b64339dc67b6b5ebc
OdinEM <e.oden@alustudent.com>
2024-09-27 20:01:06 +0200

    test

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,19 +1,25 @@
-﻿﻿using System.Collections;
+﻿﻿using System.Collections.Generic;
+using System;
 
 class List{
 
     public static List<int> CreatePrint(int size){
         
-        if(size < 0){
+        if (size < 0){
             Console.WriteLine("Size cannot be negative");
             return null;
-        }else{
+        } else {
             List<int> CacheList = new List<int>();
-            for(int i = 0; i < size ; i ++){
-                Console.Write(i + " ");
+            for (int i = 0; i < size; i++){
+                // Print the number followed by space, except for the last element
+                if (i == size - 1) {
+                    Console.Write(i);  // Last element, no trailing space
+                } else {
+                    Console.Write(i + " ");
+                }
                 CacheList.Add(i);
             }
-            Console.WriteLine();
+            Console.WriteLine();  // Print a newline after the loop finishes
             return CacheList;
         }
     }

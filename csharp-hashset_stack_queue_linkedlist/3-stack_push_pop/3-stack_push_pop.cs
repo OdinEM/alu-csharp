68b45e7d02f424e45a38f9a9e5aced680a2e60ef
OdinEM <e.oden@alustudent.com>
2024-09-28 21:23:22 +0200

    three

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class MyStack<T>
+class MyStack
 {
-    private List<T> stackList = new List<T>();
-
-    public int Count()
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        return stackList.Count;
-    }
+        // Step 1: Print the number of items in the stack
+        Console.WriteLine($"Number of items: {aStack.Count}");
 
-    public void Push(T obj)
-    {
-        stackList.Add(obj);
-    }
-
-    public T Pop()
-    {
-        if (stackList.Count == 0)
+        // Step 2: Print the top item of the stack without removing it
+        if (aStack.Count == 0)
         {
-            return default(T);
+            Console.WriteLine("Stack is empty");
         }
-        T item = stackList[stackList.Count - 1];
-        stackList.RemoveAt(stackList.Count - 1);
-        return item;
-    }
-}
+        else
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        MyStack<int> stack = new MyStack<int>();
-        stack.Push(1);
-        Console.WriteLine(stack.Count());
+        // Step 3: Check if the stack contains the search item
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
+
+        // Step 4: If the stack contains the search item, remove items up to and including it
+        if (containsSearch)
+        {
+            // Store items in a temporary stack (since we can only Pop once)
+            Stack<string> tempStack = new Stack<string>();
+
+            // Pop items from the original stack into the temp stack until we find the search item
+            string poppedItem = aStack.Pop(); // This is the only Pop we are allowed to use
+
+            tempStack.Push(poppedItem); // Push the first popped item to tempStack
+
+            while (poppedItem != search && aStack.Count > 0)
+            {
+                poppedItem = aStack.Pop(); // Pop more items until we find the search item
+                tempStack.Push(poppedItem); // Keep pushing items into the temporary stack
+            }
+
+            // Now, the search item has been popped, and we stop here.
+            // No need to return these items back to the original stack.
+        }
+
+        // Step 5: Add the new item to the stack
+        aStack.Push(newItem);
+
+        // Step 6: Return the modified stack
+        return aStack;
     }
 }

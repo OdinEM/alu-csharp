be2fe87c30802dd6028a5ba6f7367fd8697e3629
OdinEM <e.oden@alustudent.com>
2024-10-19 17:01:13 +0200

    ts

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,39 +1,15 @@
-using NUnit.Framework;
-using MyMath;
+namespace MyMath.Tests;
 
-namespace MyMath.Tests
+public class Tests
 {
-    [TestFixture]
-    public class OperationsTests
+    [SetUp]
+    public void Setup()
     {
-        [Test]
-        public void Add_TwoPositiveIntegers_ReturnsCorrectSum()
-        {
-            Assert.That(Operations.Add(5, 7), Is.EqualTo(12));
-        }
+    }
 
-        [Test]
-        public void Add_TwoNegativeIntegers_ReturnsCorrectSum()
-        {
-            Assert.That(Operations.Add(-5, -7), Is.EqualTo(-12));
-        }
-
-        [Test]
-        public void Add_PositiveAndNegativeIntegers_ReturnsCorrectSum()
-        {
-            Assert.That(Operations.Add(5, -3), Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Add_ZeroAndPositiveInteger_ReturnsPositiveInteger()
-        {
-            Assert.That(Operations.Add(0, 5), Is.EqualTo(5));
-        }
-
-        [Test]
-        public void Add_TwoZeros_ReturnsZero()
-        {
-            Assert.That(Operations.Add(0, 0), Is.EqualTo(0));
-        }
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
     }
 }

09b2d545cc9fb81adb8a4f54cce646e19dc9ee36
OdinEM <e.oden@alustudent.com>
2024-11-16 21:16:51 +0200

    green

---
+++ csharp-linear_algebra/3-magnitude/main_0.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] vector = new double[] { 3, -7 };
+        Console.WriteLine(VectorMath.Magnitude(vector));
+    }
+}

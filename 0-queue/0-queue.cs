3c1c4ad1ecb1e55d8797a21a7d9811d3f83b667f
OdinEM <e.oden@alustudent.com>
2025-02-23 17:32:28 +0200

    first project

---
+++ 0-queue/0-queue.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Generic Queue class that can store items of any type
+/// </summary>
+/// <typeparam name="T">The type of elements stored in the queue</typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// Returns the Queue's type parameter
+    /// </summary>
+    /// <returns>The type of T</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}

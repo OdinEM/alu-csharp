d6114ebf817a9d2738a619ad9e6b015554eed369
OdinEM <e.oden@alustudent.com>
2025-03-02 18:21:30 +0200

    all working clean

--- csharp-hello_world/0-new_project/obj/Debug/net7.0/0-new_project.AssemblyInfo.cs
+++ csharp-hello_world/0-new_project/obj/Debug/net7.0/0-new_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-new_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e96591ad311071a82497d36d4819e779b1b1d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e012a208da656fceb0a7d8f97b5368ebe054cefc")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-new_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-new_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
